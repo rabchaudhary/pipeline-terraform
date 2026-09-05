27a739884d1d8b695b506ba7f3363f36df5969cc
rabi <rabicloud14@gmail.com>
2026-09-05 16:04:32 +0530

    modified the code

--- parent_module/provider.tf
+++ parent_module/provider.tf
@@ -2,11 +2,16 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "5.2.0"
+      version = "=5.0.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+
+  subscription_id = "5e7275ad-fc65-40a2-8897-2194edb50882"
+  tenant_id       = "c1ef35fa-feb3-4ab0-888c-c6778cb1e96d"
+
+  use_cli = false
 }
